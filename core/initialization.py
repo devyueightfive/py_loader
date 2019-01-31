897843492bf4a75e66850578018ea78747b9a5a0
yuri <dev.yu85@yandex.ru>
2019-01-31 15:08:50 +0300

    Commented initializeDatabase function

--- core/initialization.py
+++ core/initialization.py
@@ -5,15 +5,22 @@ import tables
 from core.settings import settings, saveSettings, DescriptionForTradesTable
 
 
+# Checking existence of database.
+# In case of absence create new one.
 def initializeDatabase():
+    # Get path to database from settings.json.
     fullPathToDatabase = settings['pathToDatabase']
+    # In case of wrong path to database
     if not os.path.isfile(path=fullPathToDatabase):
+        # use default path to database.
         fullPathToDatabase = os.path.join(os.path.abspath('.'), settings['defaultPathToDatabase'])
         pathToDatabase = os.path.dirname(fullPathToDatabase)
+        # In case of absence database
         if not os.path.exists(pathToDatabase):
             os.makedirs(pathToDatabase)
+        # create new database.
         with tables.open_file(filename=fullPathToDatabase, mode='w', title='Crypto Trades') as hdf5Base:
-            'create groups'
+            # Use settings.json parameters for database structure.
             for pair in settings['tradePairs']:
                 pairGroup = hdf5Base.create_group('/', pair)
                 for mu in settings['marketURLs']:
@@ -21,11 +28,13 @@ def initializeDatabase():
                     table = hdf5Base.create_table(urlGroup, 'trades', DescriptionForTradesTable,
                                                   title="Trades")
                     table.close()
-        # end with
+        # //end with
 
-        # save settings in settings.json
+        # Save new parameters in settings.json.
         saveSettings('pathToDatabase', fullPathToDatabase)
-    # return value  for tests
+    # //end if
+
+    # Return new parameter for tests
     return fullPathToDatabase
 
 
