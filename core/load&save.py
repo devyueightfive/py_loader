c9ad001436b2232035c860c7f4f322e7a27e4c17
yuri <dev.yu85@yandex.ru>
2019-02-03 03:14:15 +0300

    added general structure

--- core/load&save.py
+++ core/load&save.py
@@ -1,13 +1,18 @@
 from queue import Queue
 
+import tables
+
 from core.database.initialization import initialization
+from core.database.saver import MarketDataLoader
 
 # Body of Main program
 if __name__ == "__main__":
     # make database initialization
-    initialization()
+    pathToH5 = initialization()
     # shared queue for
     # Threads that load data from markets (ninja)
     # and
     # Thread that saves data to database (samurai)
     dataQueue = Queue()
+    h5file = tables.open_file(filename=pathToH5, mode='w', title='Crypto Trades')
+    samurai = MarketDataLoader(dataQueue, h5file)
